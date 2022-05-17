7ef7f325bf100d2a904b19c99dbd8c0753d4bd16
kelvinmozart <kelvin.mozart@live.com>
2022-05-17 14:51:37 -0300

    aula 3 testes com jest finalizados

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "5de942cb5cd12d",
+        pass: "9a55c5dab1ba38"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Kelvin Mozart <kelvin.mozart@live.com>',
+            subject,
+            html: body,
+        });
+    }
+}
